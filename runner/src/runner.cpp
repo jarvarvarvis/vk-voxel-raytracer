d87621eb9336ee0ae8702d428c7ad99f37866886
JaJo <jarvistrigo@gmail.com>
2022-10-31 18:43:27 +0100

    Implemented building part of the runner

--- runner/src/runner.cpp
+++ runner/src/runner.cpp
@@ -17,6 +17,34 @@ void runner::Runner::build(args::RaytracerBuildMode build_mode)
 {
     bfs::path project_path = this->raytracer_project_path;
     bfs::path build_script_path = project_path / "build.sh";
+
+    // Set build mode argument
+    std::string build_mode_arg;
+    if (build_mode == args::RaytracerBuildMode::Debug) {
+        build_mode_arg = "debug";
+    } else {
+        build_mode_arg = "release";
+    }
+
+    // If it exists, run it
+    if (bfs::exists(build_script_path)) {
+        // Run process and read its stdout to the process_in stream
+        bp::ipstream process_in;
+        bp::child build_process("/bin/bash", build_script_path, build_mode_arg, bp::std_out > process_in);
+
+        // Read process stdout
+        std::string line;
+
+        std::cout << "\nBuild script output:" << std::endl;
+        while (build_process.running()) {
+            std::getline(process_in, line);
+            std::cout << line << std::endl;
+        }
+    } else {
+        throw std::runtime_error(
+            "The raytracer build script build.sh doesn't exist in the specified project path"
+        );
+    }
 }
 
 void runner::Runner::run(args::RaytracerBuildMode build_mode)
@@ -34,13 +62,13 @@ void runner::Runner::run(args::RaytracerBuildMode build_mode)
     if (bfs::exists(executable_path)) {
         // Run process and read its stdout to the process_in stream
         bp::ipstream process_in;
-        bp::child raytracer(executable_path, bp::std_out > process_in);
+        bp::child raytracer_process(executable_path, bp::std_out > process_in);
        
-        // Read input from process
+        // Read process stdout
         std::string line;
 
         std::cout << "\nRaytracer output:" << std::endl;
-        while (raytracer.running()) {
+        while (raytracer_process.running()) {
             std::getline(process_in, line);
             std::cout << line << std::endl;
         }
